721a4e5aacae785460e50eb1a71183db4f5e1a62
pushkar79k1-commits <pushkar79k1@gmail.com>
2026-06-11 21:31:26 +0530

    Add files via upload

---
+++ QUESTION11.C
@@ -0,0 +1,19 @@
+//WAP TO FIND GCD OF TWO NUMBER
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD = %d", a);
+
+    return 0;
+}
